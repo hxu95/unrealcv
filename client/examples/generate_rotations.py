7cf39f04b093ca003a2a0dc790f2789402c16021
hxu <to.helen.xu@gmail.com>
2016-11-17 09:50:38 -0500

    updated input -> smaller vertical rotation

--- client/examples/generate_rotations.py
+++ client/examples/generate_rotations.py
@@ -4,11 +4,22 @@ from unrealcv import client
 
 trajectory = []
 
+# roll = axis through object
+# yaw = vertical axis -> left and right rotation
+# pitch = horizontal axis -> up and down rotation
+# https://sidvind.com/wiki/Yaw,_pitch,_roll_camera
 angles = [0.0, 120.0, 240.0]
+
+# 0, 30 up and down
+pitch_angles = [0.0, 30.0, 330.0]
+
+#input for (x,y,z)
 import json; camera_trajectory = json.load(open('camera_traj.json'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
+    #output = angles.json
     parser.add_argument('--filename', default='angles.json')
     args = parser.parse_args()
 
@@ -21,7 +32,7 @@ if __name__ == '__main__':
         #loc = [float(v) for v in client.request('vget /camera/0/location').split(' ')]
 
         for a in angles:
-            for b in angles:
+            for b in pitch_angles:
                 for c in angles:
                     rot = {'yaw': float(a), 'pitch': float(b), 'roll':float(c)}
                     trajectory.append(dict(rotation = rot, location = loc))
